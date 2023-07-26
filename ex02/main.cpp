86c9b1a87223b542779f142157fc39f7b1af69e2
Raehyeon Jeong <rjeong@student.42seoul.kr>
2023-07-26 18:36:45 +0900

    feat : ex02 : main

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -10,7 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
 int main() {
+	std::string str("HI THIS IS BRAIN");
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << "memory address of the string variable : " << &str << std::endl;
+	std::cout << "memory address held by stringPTR      : " << stringPTR << std::endl;
+	std::cout << "memory address held by stringREF      : " << &stringREF << std::endl;
+	std::cout << "value of the string variable          : " << str << std::endl;
+	std::cout << "value pointed to by stringPTR         : " << *stringPTR << std::endl;
+	std::cout << "value pointed to by stringREF         : " << stringREF << std::endl;
 
 	return (0);
 }
